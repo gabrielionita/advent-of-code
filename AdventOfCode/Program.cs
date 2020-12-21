bfcd52a7f6d9ec24d15a8bea7ac96ec12d2cb133
Gabriel Ioniță <gabriel.ionita@bullguard.com>
2020-12-21 18:14:37 +0200

    Added args and refactored

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -11,39 +11,60 @@ namespace AdventOfCode
 {
     public class Program
     {
-		private static DayBase day;
 		private static ILogger logger;
+		private static IServiceProvider serviceProvider;
 
-        public static async Task Main()
-        {
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
-                .AddJsonFile("appsettings.json", false)
-                .Build();
+        public static async Task Main(string[] args)
+		{
+			Init();
 
-            var services = new ServiceCollection();
-            var startup = new Startup(configuration);
-            startup.ConfigureServices(services);
-            var serviceProvider = services.BuildServiceProvider();
-            logger = serviceProvider.GetRequiredService<ILogger<Program>>();
-            logger.LogInformation("Test");
-            var newestDayType = typeof(DayBase).Assembly.GetTypes()
-                .Where(type => type.Name != nameof(DayBase) && type.Name.StartsWith("Day")).OrderByDescending(type => type.Name)
-                .First();
-            day = (DayBase)serviceProvider.GetRequiredService(newestDayType);
-            logger.LogInformation($"Running {newestDayType.Name}");
-
-            try
+			try
 			{
-				await Run();
+				var day = GetDay(args);
+				await Run(logger, day);
 			}
 			catch (Exception exception)
 			{
-                logger.LogError(exception, "Error");
+				logger.LogError(exception, "Error");
 			}
-        }
+		}
 
-		private static async Task Run()
+		private static void Init()
+		{
+			var configuration = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+				.AddJsonFile("appsettings.json", false)
+				.Build();
+
+			var services = new ServiceCollection();
+			var startup = new Startup(configuration);
+			startup.ConfigureServices(services);
+			serviceProvider = services.BuildServiceProvider();
+			logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+		}
+
+		private static DayBase GetDay(string[] args)
+		{
+			Type dayType;
+			if (args[0] == "--latest-day")
+			{
+				dayType = typeof(DayBase).Assembly.GetTypes()
+					.Where(type => type.Name != nameof(DayBase) && type.Name.StartsWith("Day")).OrderByDescending(type => type.Name)
+					.First();
+			}
+			else if (args[0] == "--day" && int.TryParse(args[1], out var number))
+			{
+				dayType = Type.GetType($"AdventOfCode.Days.Day{number:00}, AdventOfCode.Days", true);
+			}
+			else
+			{
+				throw new ArgumentException("Args are not specified");
+			}
+			logger.LogInformation($"Running {dayType.Name}");
+			return (DayBase)serviceProvider.GetRequiredService(dayType);
+		}
+
+		private static async Task Run(ILogger logger, DayBase day)
 		{
 			var input = await day.GetInput();
 			day.SolvePart1(input);
