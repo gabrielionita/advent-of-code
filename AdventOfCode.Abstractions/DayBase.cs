bf027fcbe1015781410f39412f7ffa3dd439f2c3
Gabriel Ioniță <gabrielionita@live.com>
2020-12-29 00:10:20 +0200

    Removed useless logger

--- AdventOfCode.Abstractions/DayBase.cs
+++ AdventOfCode.Abstractions/DayBase.cs
@@ -1,21 +1,19 @@
-﻿using Microsoft.Extensions.Logging;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace AdventOfCode.Abstractions
 {
 	public abstract class DayBase<TInput, TSolution>
     {
-		private readonly HttpClient httpClient;
-		private readonly ILogger logger;
 		private readonly int day;
         private readonly int year;
+		private readonly HttpClient httpClient;
 
 		protected DayBase(HttpClient httpClient)
 		{
 			this.httpClient = httpClient;
             var type = GetType();
-			day = int.Parse(type.Name[3..]);
+			day = int.Parse(type.Name.Substring(3));
             year = int.Parse(type.FullName.Substring(type.FullName.IndexOf("Days") + 4, 4));
 		}
 
