b9ff0026f3ca72f0d1b9e9a045330a3c6c7b065c
Gabriel Ionita <gabrielionita@live.com>
2022-01-09 19:50:43 +0200

    Added 2021 Day01

---
+++ AdventOfCode.Days/2021/Day01.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode.Days2021
+{
+    public class Day01 : DayBase<int[], int>
+    {
+        public override int[] MapInput(string input) => input.Split('\n', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+        public override int SolvePart1(int[] input) => GenericSolution(input, 1);
+
+        public override int SolvePart2(int[] input) => GenericSolution(input, 3);
+
+        private int GenericSolution(int[] input, int windowSize)
+        {
+            var increases = 0;
+            var previousSum = int.MaxValue;
+            for (var i = windowSize - 1; i < input.Length; i++)
+            {
+                var sum = 0;
+                for (var j = 0; j < windowSize; j++)
+                    sum += input[i - j];
+
+                if (previousSum < sum)
+                    increases++;
+
+                previousSum = sum;
+            }
+
+            return increases;
+        }
+    }
+}
