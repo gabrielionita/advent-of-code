934cb4da6e270a966b68e598363291774090bfe1
Gabriel Ionita <gabrielionita@live.com>
2022-09-28 15:32:05 +0300

    Added Day03 and partial Day04

---
+++ AdventOfCode.Days/2021/Day03.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode.Days2021
+{
+    public class Day03 : DayBase<char[][], int>
+    {
+        public override char[][] MapInput(string input)
+        {
+            return input.Split('\n', System.StringSplitOptions.RemoveEmptyEntries).Select(c => c.ToCharArray()).ToArray();
+        }
+
+        private int Convert(int[] bits)
+        {
+            var result = 0;
+            for (var i = 0; i < bits.Length; i++)
+            {
+                if (bits[i] == 1)
+                    result += (int)Math.Pow(2, bits.Length - i - 1);
+            }
+
+            return result;
+        }
+
+        public override int SolvePart1(char[][] input)
+        {
+            var columns = input[0].Length;
+            var rows = input.Length;
+            var gammaRate = new int[columns];
+            var epsilonRate = new int[columns];
+            for (var i = 0; i < columns; i++)
+            {
+                var oneCount = 0;
+                for (var j = 0; j < rows; j++)
+                {
+                    if (input[j][i] == '1')
+                        oneCount++;
+                }
+
+                gammaRate[i] = oneCount > rows / 2 ? 1 : 0;
+                epsilonRate[i] = gammaRate[i] == 0 ? 1 : 0;
+            }
+
+            return Convert(gammaRate) * Convert(epsilonRate);
+
+        }
+
+        public override int SolvePart2(char[][] input)
+        {
+            var inputClone = (char[][])input.Clone();
+            var columns = inputClone[0].Length;
+            for (var i = 0; i < columns; i++)
+            {
+                var rows = inputClone.Length;
+                if (rows == 1)
+                    break;
+
+                var oneCount = 0;
+                for (var j = 0; j < rows; j++)
+                {
+                    if (inputClone[j][i] == '1')
+                        oneCount++;
+                }
+
+                var bitToKeep = oneCount < rows - oneCount ? '0' : '1';
+                inputClone = inputClone.Where(c => c[i] == bitToKeep).ToArray();
+            }
+            var oxygenGeneratorRating = Convert(inputClone[0].Select(c => int.Parse(c.ToString())).ToArray());
+
+            inputClone = (char[][])input.Clone();
+            for (var i = 0; i < columns; i++)
+            {
+                var rows = inputClone.Length;
+                if (rows == 1)
+                    break;
+
+                var oneCount = 0;
+                for (var j = 0; j < rows; j++)
+                {
+                    if (inputClone[j][i] == '1')
+                        oneCount++;
+                }
+
+                var bitToKeep = oneCount >= rows - oneCount ? '0' : '1';
+                inputClone = inputClone.Where(c => c[i] == bitToKeep).ToArray();
+            }
+
+            var co2ScrubberRating = Convert(inputClone[0].Select(c => int.Parse(c.ToString())).ToArray());
+
+            return oxygenGeneratorRating * co2ScrubberRating;
+        }
+    }
+}
---
+++ AdventOfCode.Days/2021/Day04.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Days2021
+{
+    public class Day04 : DayBase<DataDay04, int>
+    {
+        public override DataDay04 MapInput(string input)
+        {
+            var lines = input.Split('\n');
+            var data = new DataDay04
+            {
+                Numbers = lines[0].Split(',').Select(int.Parse).ToArray()
+            };
+            var boards = new List<int[][]>();
+            for (var i = 2; i < lines.Length; i++)
+            {
+                var board = new int[5][];
+                if (string.IsNullOrEmpty(lines[i]))
+                {
+                    continue;
+                }
+
+                for (var j = 0; j < 5; j++)
+                {
+                    board[j] = lines[i + j].Split(' ', System.StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                }
+                i += 5;
+                boards.Add(board);
+            }
+            data.Boards = boards.ToArray();
+            return data;
+        }
+
+        public override int SolvePart1(DataDay04 input)
+        {
+            foreach (var number in input.Numbers)
+            {
+                foreach (var board in input.Boards)
+                {
+                    for (var i = 0; i < board.Length; i++)
+                    {
+                        for (var j = 0; j < board.GetLength(0); j++)
+                        {
+                            if (board[i,j] == number)
+                            {
+                                board[i,j] = 0;
+                            }
+                        }
+                    }
+
+                    var win = true;
+                    for (var i = 0; i < board.Length; i++)
+                    {
+                        win = true;
+                        for (var j = 0; j < board.GetLength(1); j++)
+                            if (board[i,j] != 0)
+                            {
+                                win = false;
+                                break;
+                            }
+
+
+                        if (win)
+                            break;
+                    }
+
+                    if (!win)
+                    {
+                        for (var i = 0; i < board.GetLength(1); i++)
+                        {
+                            win = true;
+                            for (var j = 0; j < board.GetLength(0); j++)
+                                if (board[j,i] != 0)
+                                {
+                                    win = false;
+                                    break;
+                                }
+
+                            if (win)
+                                break;
+                        }
+                    }
+
+                    if (win)
+                    {
+                        var sum = 0;
+                        for(var i = 0; i < board.GetLength(0); i++)
+                            for(var j = 0; j < board.GetLength(1); j++)
+                                sum += board[i,j];
+
+                        return sum * number;
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        public override int SolvePart2(DataDay04 input)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class DataDay04
+    {
+        public int[] Numbers { get; set; }
+        public int[][,] Boards { get; set; }
+    }
+}
