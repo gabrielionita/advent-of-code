82d39a731d684159697650c97029c7a9fae5faef
Gabriel Ioniță <gabrielionita@live.com>
2022-09-29 23:17:23 +0300

    Finished Day04

--- AdventOfCode.Days/2021/Day04.cs
+++ AdventOfCode.Days/2021/Day04.cs
@@ -32,64 +32,77 @@ namespace AdventOfCode.Days2021
             return data;
         }
 
+
+        private void MarkBoard(int[][] board, int number)
+        {
+            for (var i = 0; i < board.Length; i++)
+                for (var j = 0; j < board[i].Length; j++)
+                    if (board[i][j] == number)
+                        board[i][j] = -1;
+        }
+
+        private bool AnyRowIsMarked(int[][] board)
+        {
+            var marked = true;
+            for (var i = 0; i < board.Length; i++)
+            {
+                marked = true;
+                for (var j = 0; j < board[i].Length; j++)
+                    if (board[i][j] != -1)
+                    {
+                        marked = false;
+                        break;
+                    }
+
+
+                if (marked)
+                    break;
+            }
+            return marked;
+        }
+
+        private bool AnyColumnIsMarked(int[][] board)
+        {
+            var marked = true;
+            for (var i = 0; i < board.Length; i++)
+            {
+                marked = true;
+                for (var j = 0; j < board[i].Length; j++)
+                    if (board[j][i] != -1)
+                    {
+                        marked = false;
+                        break;
+                    }
+
+
+                if (marked)
+                    break;
+            }
+            return marked;
+        }
+
+        private int CalculateScore(int[][] board, int number)
+        {
+            var sum = 0;
+            for (var i = 0; i < board.Length; i++)
+                for (var j = 0; j < board[i].Length; j++)
+                    if (board[i][j] != -1)
+                        sum += board[i][j];
+
+            return sum * number;
+        }
+
         public override int SolvePart1(DataDay04 input)
         {
             foreach (var number in input.Numbers)
             {
                 foreach (var board in input.Boards)
                 {
-                    for (var i = 0; i < board.Length; i++)
+                    MarkBoard(board, number);
+
+                    if (AnyRowIsMarked(board) || AnyColumnIsMarked(board))
                     {
-                        for (var j = 0; j < board.GetLength(0); j++)
-                        {
-                            if (board[i,j] == number)
-                            {
-                                board[i,j] = 0;
-                            }
-                        }
-                    }
-
-                    var win = true;
-                    for (var i = 0; i < board.Length; i++)
-                    {
-                        win = true;
-                        for (var j = 0; j < board.GetLength(1); j++)
-                            if (board[i,j] != 0)
-                            {
-                                win = false;
-                                break;
-                            }
-
-
-                        if (win)
-                            break;
-                    }
-
-                    if (!win)
-                    {
-                        for (var i = 0; i < board.GetLength(1); i++)
-                        {
-                            win = true;
-                            for (var j = 0; j < board.GetLength(0); j++)
-                                if (board[j,i] != 0)
-                                {
-                                    win = false;
-                                    break;
-                                }
-
-                            if (win)
-                                break;
-                        }
-                    }
-
-                    if (win)
-                    {
-                        var sum = 0;
-                        for(var i = 0; i < board.GetLength(0); i++)
-                            for(var j = 0; j < board.GetLength(1); j++)
-                                sum += board[i,j];
-
-                        return sum * number;
+                        return CalculateScore(board, number);
                     }
                 }
             }
@@ -99,13 +112,33 @@ namespace AdventOfCode.Days2021
 
         public override int SolvePart2(DataDay04 input)
         {
-            throw new System.NotImplementedException();
+            var winningBoards = new List<int>();
+            foreach(var number in input.Numbers)
+            {
+                for(var boardIndex = 0; boardIndex < input.Boards.Length; boardIndex++)
+                {
+                    if (winningBoards.Contains(boardIndex))
+                        continue;
+                    var board = input.Boards[boardIndex];
+
+                    MarkBoard(board, number);
+
+                    if (AnyRowIsMarked(board) || AnyColumnIsMarked(board))
+                    {
+                        winningBoards.Add(boardIndex);
+                        if(winningBoards.Count == input.Boards.Length)
+                            return CalculateScore(board, number);
+                    }
+                }
+            }
+
+            return 0;
         }
     }
 
     public class DataDay04
     {
         public int[] Numbers { get; set; }
-        public int[][,] Boards { get; set; }
+        public int[][][] Boards { get; set; }
     }
 }
