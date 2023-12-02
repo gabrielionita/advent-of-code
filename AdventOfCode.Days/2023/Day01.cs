e0d3c4aca6ae3b37a31677eb7bb28244459361bc
Gabriel Ionita <gabrielionita@live.com>
2023-12-02 13:44:37 +0200

    Added Days 01 and 02 of 2023

---
+++ AdventOfCode.Days/2023/Day01.cs
@@ -0,0 +1,94 @@
+﻿using System.Linq;
+
+namespace AdventOfCode.Days2023
+{
+    public class Day01 : DayBase<string[], int>
+    {
+        private readonly string[] numbersAsSrings = new[]
+        {
+             "one",
+             "two",
+             "three",
+             "four",
+             "five",
+             "six",
+             "seven",
+             "eight",
+             "nine"
+        };
+
+        public override string[] MapInput(string input)
+        {
+            return input.Split('\n', System.StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public override int SolvePart1(string[] input)
+        {
+            var sum = 0;
+            foreach(var line in input)
+            {
+                var firstNumber = line.First(c => char.IsDigit(c));
+                var lastNumber = line.Last(c => char.IsDigit(c));
+                var number = int.Parse($"{firstNumber}{lastNumber}");
+                sum += number;
+            }
+
+            return sum;
+        }
+
+        public override int SolvePart2(string[] input)
+        {
+            var sum = 0;
+            foreach(var line in input)
+            {
+                var firstNumber = line.First(c => char.IsDigit(c));
+                var firstNumberIndex = line.IndexOf(firstNumber);
+
+                var firstNumberStringIndex = line.Length;
+                var firstNumberString = 10;
+                for(var i = 0; i < numbersAsSrings.Length; i++)
+                {
+                    var stringNumber = numbersAsSrings[i];
+                    var index = line.IndexOf(stringNumber);
+                    if(index != - 1 && index < firstNumberStringIndex)
+                    {
+                        firstNumberStringIndex = index;
+                        firstNumberString = i + 1;
+                    }
+                }
+               
+                if(firstNumberIndex > firstNumberStringIndex)
+                {
+                    firstNumber = firstNumberString.ToString()[0];
+                }
+
+
+                var lastNumber = line.Last(c => char.IsDigit(c));
+                var lastNumberIndex = line.LastIndexOf(lastNumber);
+
+                var lastNumberStringIndex = -1;
+                var lastNumberString = -1;
+                for (var i = 0; i < numbersAsSrings.Length; i++)
+                {
+                    var stringNumber = numbersAsSrings[i];
+                    var index = line.LastIndexOf(stringNumber);
+                    if (index > lastNumberStringIndex)
+                    {
+                        lastNumberStringIndex = index;
+                        lastNumberString = i + 1;
+                    }
+                }
+
+                if (lastNumberString != -1 && lastNumberIndex < lastNumberStringIndex)
+                {
+                    lastNumber = lastNumberString.ToString().ToCharArray().First();
+                }
+
+                var number = int.Parse($"{firstNumber}{lastNumber}");
+                sum += number;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ AdventOfCode.Days/2023/Day02.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Days2023
+{
+    public class Day02 : DayBase<Dictionary<int, Dictionary<string, int>>, long>
+    {
+        public override Dictionary<int, Dictionary<string, int>> MapInput(string input)
+        {
+            var lines = input.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+            var result = new Dictionary<int, Dictionary<string, int>>();
+            foreach (var line in lines)
+            {
+                var indexOfFirstSpace = line.IndexOf(" ");
+                var indexOfFirstColon = line.IndexOf(":");
+                var game = int.Parse(line.Substring(indexOfFirstSpace, indexOfFirstColon - indexOfFirstSpace));
+
+                var dictionary = new Dictionary<string, int>();
+                var sets = line.Substring(indexOfFirstColon + 1).Split(";");
+                foreach (var set in sets)
+                {
+                    var groups = set.Split(',');
+                    foreach (var group in groups)
+                    {
+                        var items = group.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                        var number = int.Parse(items[0]);
+                        var color = items[1];
+                        if (dictionary.ContainsKey(color) &&  dictionary[color] < number || !dictionary.ContainsKey(color))
+                        {
+                            dictionary[color] = number;
+                        }
+                    }
+
+                }
+
+                result.Add(game, dictionary);
+            }
+
+            return result;
+        }
+
+        public override long SolvePart1(Dictionary<int, Dictionary<string, int>> games)
+        {
+            var dictionary = new Dictionary<string, int>
+            {
+                { "red", 12 },
+                { "green", 13 },
+                { "blue", 14 },
+            };
+
+            var sum = 0;
+            foreach(var game in games)
+            {
+                var possible = true;
+                foreach(var cube in dictionary)
+                {
+                    if (game.Value[cube.Key] > cube.Value)
+                    {
+                        possible = false; 
+                        break;
+                    }
+                }
+                if (possible)
+                {
+                    sum += game.Key;
+                }
+            }
+            return sum;
+        }
+
+        public override long SolvePart2(Dictionary<int, Dictionary<string, int>> games)
+        {
+            var sum = 0;
+            foreach(var game in games)
+            {
+                var power = game.Value.Values.Aggregate(1, (a, b) => a * b);
+                sum += power;
+            }
+
+            return sum;
+        }
+    }
+}
