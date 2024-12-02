75d1296e1a6ea173f2ec6b341f96c51cfb3b454f
Gabriel Ionita <gabrielionita@live.com>
2024-12-02 17:41:28 +0200

    Added Days 1 and 2 from 2024

---
+++ AdventOfCode.Days/2024/Day01.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Days2024
+{
+    public class Day01 : IDay<(IEnumerable<int>, IEnumerable<int>), int>
+    {
+        public (IEnumerable<int>, IEnumerable<int>) MapInput(string input)
+        {
+            var list1 = new List<int>();
+            var list2 = new List<int>();
+            var lines = input.Split('\n', StringSplitOptions.RemoveEmptyEntries).ToArray();
+            foreach (var line in lines)
+            {
+                var numbers = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                list1.Add(numbers[0]);
+                list2.Add(numbers[1]);
+            }
+
+            return (list1, list2);  
+        }
+
+        public int SolvePart1((IEnumerable<int>, IEnumerable<int>) input)
+        {
+            var orderedZip = input.Item1.OrderBy(x => x).Zip(input.Item2.OrderBy(x => x));
+            var sum = 0;
+            foreach (var (first, second) in orderedZip)
+            {
+                var diff = Math.Abs(first - second);
+                sum += diff;
+            }
+
+            return sum;
+        }
+
+        public int SolvePart2((IEnumerable<int>, IEnumerable<int>) input)
+        {
+            var similarity = 0;
+            foreach(var item in input.Item1)
+            {
+                var count = input.Item2.Count(x => x == item);
+                similarity += item * count;
+            }
+
+            return similarity;
+        }
+    }
+}
---
+++ AdventOfCode.Days/2024/Day02.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode.Days2024
+{
+    public class Day02 : IDay<int[][], int>
+    {
+        public int[][] MapInput(string input)
+        {
+            var lines = input.Split('\n', StringSplitOptions.RemoveEmptyEntries).ToArray();
+            var matrix = new int[lines.Length][];
+            for (var i = 0; i < lines.Length; i++)
+            {
+                var numbers = lines[i].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                matrix[i] = numbers;
+            }
+
+            return matrix;
+        }
+
+        public int SolvePart1(int[][] input)
+        {
+            var safeReports = 0;
+            for (var row = 0; row < input.Length; row++)
+            {
+                var safe = IsMonotonous(input[row]) && Diff(input[row]);
+                if (safe)
+                {
+                    safeReports++;
+                }
+            }
+
+            return safeReports;
+        }
+
+        public int SolvePart2(int[][] input)
+        {
+            var safeReports = 0;
+            for (var row = 0; row < input.Length; row++)
+            {
+                if (IsMonotonous(input[row]) && Diff(input[row]))
+                {
+                    safeReports++;
+                    continue;
+                }
+
+                for (var column = 0; column < input[row].Length; column++)
+                {
+                    var excludedColumn = input[row][0..column].Concat(input[row][(column + 1)..]).ToArray();
+                    if (IsMonotonous(excludedColumn) && Diff(excludedColumn))
+                    {
+                        safeReports++;
+                        break;
+                    }
+                }
+            }
+
+            return safeReports;
+        }
+
+        private bool IsMonotonous(int[] input)
+        {
+            var increasing = input[0] < input[1];
+            for (var i = 1; i < input.Length - 1; i++)
+            {
+                if (increasing && input[i] > input[i + 1])
+                {
+                    return false;
+                }
+                else if (!increasing && input[i] < input[i + 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool Diff(int[] input)
+        {
+            for (var i = 0; i < input.Length - 1; i++)
+            {
+                var diff = Math.Abs(input[i] - input[i + 1]);
+                if (diff < 1 || diff > 3)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
